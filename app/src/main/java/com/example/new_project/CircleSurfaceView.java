7f0543c98d012c27bf904c689e8dabdcd4990d32
nakagawt22 <nakagawt22@up.edu>
2021-09-08 12:08:19 -0700

    done for day

--- app/src/main/java/com/example/new_project/CircleSurfaceView.java
+++ app/src/main/java/com/example/new_project/CircleSurfaceView.java
@@ -3,17 +3,27 @@ package com.example.new_project;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class CircleSurfaceView {
+public class CircleSurfaceView extends SurfaceView
+implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+//Want to create paints a a memver variable for memory efficiency
 
+    private Paint redPaint;
+    showImage = true;
 }
 
 protected void onDraw(Canvas canvas)
 {
     canvas.drawCircle(300, 300, circleSize, redPaint);
-    Bitmap image = BitmapFactory.decodeResource(
-            getResources(), R.drawable.maxresdefault);
-    canvas.drawBitmap(image, 200.f, 200.f, redPaint);
+
+    if(showImage) {
+        Bitmap image = BitmapFactory.decodeResource(
+                getResources(), R.drawable.maxresdefault);
+        canvas.drawBitmap(image, 200.f, 200.f, redPaint);
+    }
 
 }
 @Override
@@ -25,6 +35,24 @@ public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
     //B: We have to tell the view that it is no longer valid
     invalidate();
 }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    //WANT to show or hide this image
+        showImage = !showImage;
+        invalidate();
+    }
+
 
 
 
